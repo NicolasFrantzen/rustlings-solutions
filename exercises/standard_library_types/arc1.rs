56006f3f5818dd2c087233b3d0ee62713e9afc3b
Nicolas Bru Frantzen <nicolas.bru.frantzen@gmail.com>
2022-07-21 10:15:42 +0200

    Solved standard_library_types.

--- exercises/standard_library_types/arc1.rs
+++ exercises/standard_library_types/arc1.rs
@@ -18,22 +18,23 @@
 // where the second TODO comment is. Try not to create any copies of the `numbers` Vec!
 // Execute `rustlings hint arc1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 #![forbid(unused_imports)] // Do not change this, (or the next) line.
 use std::sync::Arc;
 use std::thread;
 
 fn main() {
     let numbers: Vec<_> = (0..100u32).collect();
-    let shared_numbers = // TODO
+    let shared_numbers = Arc::new(numbers);
     let mut joinhandles = Vec::new();
 
     for offset in 0..8 {
-        let child_numbers = // TODO
-        joinhandles.push(thread::spawn(move || {
-            let sum: u32 = child_numbers.iter().filter(|n| *n % 8 == offset).sum();
-            println!("Sum of offset {} is {}", offset, sum);
+        joinhandles.push(thread::spawn({
+            let child_numbers = shared_numbers.clone();
+
+            move || {
+                let sum: u32 = child_numbers.iter().filter(|n| *n % 8 == offset).sum();
+                println!("Sum of offset {} is {}", offset, sum);
+            }
         }));
     }
     for handle in joinhandles.into_iter() {
